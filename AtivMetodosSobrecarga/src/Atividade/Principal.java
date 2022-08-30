d63e95e92dce7f98fde0fe597a88deaf40a650ae
Kalleb <souzakalleb73@gmail.com>
2022-08-30 13:13:03 -0300

    atividade do livro

---
+++ AtivMetodosSobrecarga/src/Atividade/Principal.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Atividade;
+
+/**
+ * @author Sr0
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Quadrilatero quadrilatero = new Quadrilatero();
+		System.out.println(quadrilatero.calcularArea(12));
+		System.out.println(quadrilatero.calcularArea(12, 14));
+		System.out.println(quadrilatero.calcularArea(2, 4));
+		System.out.println(quadrilatero.calcularArea(5, 6, 5));
+		
+
+	}
+
+}
